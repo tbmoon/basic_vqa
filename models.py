344d084d4c12c2acb60d39bc02494e65b1e974a2
tbmoon <taebong.moon87@gmail.com>
2019-04-19 12:38:58 -0400

    PEP 8

--- models.py
+++ models.py
@@ -4,76 +4,71 @@ import torchvision.models as models
 
 
 class ImgEncoder(nn.Module):
-    
     def __init__(self, embed_size):
         """(1) Load the pretrained model as you want.
                cf) one needs to check structure of model using 'print(model)'
-                   to remove last fc layer from the model. 
-           (2) Replace final fc layer (score values from the ImageNet) 
+                   to remove last fc layer from the model.
+           (2) Replace final fc layer (score values from the ImageNet)
                with new fc layer (image feature).
            (3) Normalize feature vector.
         """
         super(ImgEncoder, self).__init__()
         model = models.vgg16(pretrained=True)
-        in_features = model.classifier[-1].in_features # input size of feature vector 
+        in_features = model.classifier[-1].in_features  # input size of feature vector
         model.classifier = nn.Sequential(
-            *list(model.classifier.children())[:-1])   # remove last fc layer
-        
-        self.model = model                             # loaded model without last fc layer
-        self.fc = nn.Linear(in_features, embed_size)   # feature vector of image
-                
+            *list(model.classifier.children())[:-1])    # remove last fc layer
+
+        self.model = model                              # loaded model without last fc layer
+        self.fc = nn.Linear(in_features, embed_size)    # feature vector of image
+
     def forward(self, image):
         """Extract feature vector from image vector.
         """
         with torch.no_grad():
-            img_feature = self.model(image) # [batch_size, vgg16_fc=4096]
-        img_feature = self.fc(img_feature)  # [batch_size, embed_size]
+            img_feature = self.model(image)  # [batch_size, vgg16_fc=4096]
+        img_feature = self.fc(img_feature)   # [batch_size, embed_size]
 
         return img_feature
 
-    
+
 class QstEncoder(nn.Module):
-    
     def __init__(self, qst_vocab_size, word_embed_size, embed_size, num_layers, hidden_size):
-        
-        super(QstEncoder, self).__init__()        
-        self.word2vec = nn.Embedding(qst_vocab_size, word_embed_size) 
+        super(QstEncoder, self).__init__()
+        self.word2vec = nn.Embedding(qst_vocab_size, word_embed_size)
         self.tanh = nn.Tanh()
         self.lstm = nn.LSTM(word_embed_size, hidden_size, num_layers)
         self.relu = nn.ReLU()
-        self.fc = nn.Linear(2*num_layers*hidden_size, embed_size)    # 2 for hidden and cell states
-                
+        self.fc = nn.Linear(2*num_layers*hidden_size, embed_size)     # 2 for hidden and cell states
+
     def forward(self, question):
-        
-        qst_vec = self.word2vec(question)                            # [batch_size, max_qst_length=30, word_embed_size=300]
+
+        qst_vec = self.word2vec(question)                             # [batch_size, max_qst_length=30, word_embed_size=300]
         qst_vec = self.tanh(qst_vec)
-        qst_vec = qst_vec.transpose(0, 1)                            # [max_qst_length=30, batch_size, word_embed_size=300]
-        _, (hidden, cell) = self.lstm(qst_vec)                       # [num_layers=2, batch_size, hidden_size=512]
-        qst_feature = torch.cat((hidden, cell), 2)                   # [num_layers=2, batch_size, 2*hidden_size=1024]
-        qst_feature = qst_feature.transpose(0, 1)                    # [batch_size, num_layers=2, 2*hidden_size=1024]
-        qst_feature = qst_feature.reshape(qst_feature.size()[0], -1) # [batch_size, 2*num_layers*hidden_size=2048]
+        qst_vec = qst_vec.transpose(0, 1)                             # [max_qst_length=30, batch_size, word_embed_size=300]
+        _, (hidden, cell) = self.lstm(qst_vec)                        # [num_layers=2, batch_size, hidden_size=512]
+        qst_feature = torch.cat((hidden, cell), 2)                    # [num_layers=2, batch_size, 2*hidden_size=1024]
+        qst_feature = qst_feature.transpose(0, 1)                     # [batch_size, num_layers=2, 2*hidden_size=1024]
+        qst_feature = qst_feature.reshape(qst_feature.size()[0], -1)  # [batch_size, 2*num_layers*hidden_size=2048]
         qst_feature = self.relu(qst_feature)
-        qst_feature = self.fc(qst_feature)                           # [batch_size, embed_size]
+        qst_feature = self.fc(qst_feature)                            # [batch_size, embed_size]
 
         return qst_feature
-    
-    
+
+
 class VqaModel(nn.Module):
-    
     def __init__(self, embed_size, qst_vocab_size, ans_vocab_size, word_embed_size, num_layers, hidden_size):
-        
         super(VqaModel, self).__init__()
         self.img_encoder = ImgEncoder(embed_size)
         self.qst_encoder = QstEncoder(qst_vocab_size, word_embed_size, embed_size, num_layers, hidden_size)
         self.relu = nn.ReLU()
         self.fc = nn.Linear(embed_size, ans_vocab_size)
-                
+
     def forward(self, img, qst):
-        
-        img_feature = self.img_encoder(img)                    # [batch_size, embed_size]
-        qst_feature = self.qst_encoder(qst)                    # [batch_size, embed_size]
-        combined_feature = torch.mul(img_feature, qst_feature) # [batch_size, embed_size]
+
+        img_feature = self.img_encoder(img)                     # [batch_size, embed_size]
+        qst_feature = self.qst_encoder(qst)                     # [batch_size, embed_size]
+        combined_feature = torch.mul(img_feature, qst_feature)  # [batch_size, embed_size]
         combined_feature = self.relu(combined_feature)
-        combined_feature = self.fc(combined_feature)           # [batch_size, ans_vocab_size=1000]
-        
+        combined_feature = self.fc(combined_feature)            # [batch_size, ans_vocab_size=1000]
+
         return combined_feature
